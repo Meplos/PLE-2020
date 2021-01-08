0b036c559773109b4f2c673ce20ae9b3ed8277d1
max98hash <maxime8198@gmail.com>
2021-01-08 21:48:05 +0100

    Save id tweet + nb rt

---
+++ BatchLayer/src/main/java/bigdata/TweetSummary.java
@@ -0,0 +1,148 @@
+package bigdata;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+public class TweetSummary extends Configured implements Tool{
+
+    private static final String OUTPUT_TABLE = "gresse_tweet_summary";
+
+	public static class TweetSummaryMapper extends Mapper<LongWritable, Text, NullWritable, Text> {
+        
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+            // On parse chaque ligne en objet JSON
+            JsonParser parser = new JsonParser();
+
+            JsonObject tweetJSON = null;
+
+            try {
+                tweetJSON = parser.parse(value.toString()).getAsJsonObject();
+            } catch (Exception e) {
+                return;
+            }
+
+            try{
+                int fav = tweetJSON.get("quote_count").getAsInt();
+                int rt = tweetJSON.get("retweet_count").getAsInt();
+                int rep = tweetJSON.get("reply_count").getAsInt();
+                int quote = tweetJSON.get("favorite_count").getAsInt();
+                int total = fav+rt+rep+quote;
+                String id = tweetJSON.get("id_str").getAsString();
+
+                context.write(NullWritable.get(), new Text(id+","+rt));
+                JsonElement notDelete = tweetJSON.get("delete");
+                return;
+                
+            }catch (Exception e) {
+
+                JsonElement notDelete = tweetJSON.get("delete");
+                return;
+            }
+		}
+    }  
+    
+    public static class TweetSummaryReducer extends TableReducer<NullWritable,Text,NullWritable> {
+        
+		public void reduce(NullWritable key, Iterable<Text> values,Context context) throws IOException, InterruptedException {
+
+            for(Text tweet : values){
+
+                String[] params = tweet.toString().split(",");
+                
+                Put put = new Put(Bytes.toBytes(params[0]));
+                put.add(Bytes.toBytes("rt"),Bytes.toBytes("count") , Bytes.toBytes(params[1]));
+                context.write(NullWritable.get(), put);
+
+
+            }
+        }
+
+	}
+
+	public int run(String args[]) throws IOException, ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+        conf.set(TableOutputFormat.OUTPUT_TABLE, "gresse_tweet_summary");
+		
+		Job job = Job.getInstance(conf, "Tweets RT");
+        job.setJarByClass(TweetSummary.class);
+        
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_01_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_02_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_03_03_2020.nljson"), TextInputFormat.class);
+        /*MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_04_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_05_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_06_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_07_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_08_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_09_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_10_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_11_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_12_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_13_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_14_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_15_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_16_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_17_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_18_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_19_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_20_03_2020.nljson"), TextInputFormat.class);
+        MultipleInputs.addInputPath(job, new Path("/raw_data/tweet_21_03_2020.nljson"), TextInputFormat.class);*/
+		
+        job.setMapperClass(TweetSummaryMapper.class);
+        /*job.setOutputFormatClass(TableOutputFormat.class);*/
+		job.setMapOutputKeyClass(NullWritable.class);
+        job.setMapOutputValueClass(Text.class);
+
+        
+        TableMapReduceUtil.initTableReducerJob("gresse_tweet_summary", TweetSummaryReducer.class, job);
+        job.setReducerClass(TweetSummaryReducer.class);
+        job.setNumReduceTasks(1);
+        
+		
+		/*TableMapReduceUtil.initTableReducerJob(
+        		OUTPUT_TABLE,
+                TweetSummary.TweetSummaryReducer.class,
+                job); */ 
+		
+		return job.waitForCompletion(true) ? 0 : 1;
+	}
+
+	public static void main(String args[]) throws Exception {
+		System.exit(ToolRunner.run(new TweetSummary(), args));
+	}
+
+}
+
