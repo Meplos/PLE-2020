2192198f066ddd4953006287752e8464d0349ab0
Alexandre Erard <aerard@etu.u-bordeaux.fr>
2021-01-09 09:00:42 +0100

    Start UserStat

--- BatchLayer/src/main/java/bigdata/RTStats.java
+++ BatchLayer/src/main/java/bigdata/RTStats.java
@@ -23,35 +23,35 @@ public class RTStats {
 
         System.out.println("App Name : "+APP_NAME);
         System.out.println("HBase: "+HBASE_HOST);
-
+        int k = 10;
         SparkConf conf = new SparkConf().setAppName(APP_NAME);
         JavaSparkContext sc = new JavaSparkContext(conf);
        
         JavaRDD<String> rawtweet = sc.textFile("/raw_data/tweet_02_03_2020.nljson");
-        System.out.println(rawtweet.count());
         JavaPairRDD<Integer,String> tweetByRT = rawtweet.mapToPair(
             (tweet) -> {
-
+                
                 JsonParser parser = new JsonParser();
                 JsonObject tweetJSON = null;
                 Integer rt = -1;
-
+                
                 try {
                     tweetJSON = parser.parse(tweet).getAsJsonObject();
                     rt = new Integer(tweetJSON.get("retweet_count").getAsInt());
                     
                 } catch (Exception e) {
-                   //System.out.println("Bad Tweet");
+                    //System.out.println("Bad Tweet");
                 }
-
+                
                 return new Tuple2<Integer,String>(rt,tweet);
             }); 
-
-        //tweetByRT.sortByKey(false).take(1).forEach(x -> System.out.println(x._2));
-         tweetByRT.countByKey().forEach((x,y) -> System.out.println(x+" "+y));
-
-
-
+            JavaPairRDD topKTweet = tweetByRT.sortByKey(false).take(k);
+            //tweetByRT.sortByKey(false).take(1).forEach(x -> System.out.println(x._2));
+            //tweetByRT.countByKey().forEach((x,y) -> System.out.println(x+" "+y));
+            topKTweet.forEach(x -> System.out.println(x._1+" "+ x.x_2));
+            
+            
+            
         
 
     }
---
+++ BatchLayer/src/main/java/bigdata/UserStat.java
@@ -0,0 +1,14 @@
+package bigdata;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+/**
+ * UserStat
+ */
+public class UserStat {
+
+    public static void main(String[] args) {
+        
+    }
+}
