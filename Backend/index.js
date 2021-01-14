d6c5110c85bde9aaa214592adf48c7b5226a9a88
Alexandre Erard <aerard@etu.u-bordeaux.fr>
2021-01-14 15:04:05 +0100

    hashtag_pop + rework data word_pop

--- Backend/index.js
+++ Backend/index.js
@@ -3,12 +3,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const hbase = require("hbase");
 
-const PORT = 3000;
+const PORT = 7000;
 const client = hbase({ host: "127.0.0.1", port: 8080 });
 
 const app = express();
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -19,10 +19,23 @@ app.get("/word_pop", (req, res) => {
       batch: 100,
     },
     function (err, rows) {
-      result = rows;
-      const grouped = groupArrayOfObjects(result, "key");
-      console.log(grouped);
-      res.status(200).send(grouped);
+      result = {};
+      if (err) {
+        console.error(err);
+        res.status(400).send(err);
+        return;
+      }
+      rows.forEach((element) => {
+        if (!result[element.key]) {
+          result[element.key] = [];
+        }
+        const obj = {};
+        obj.date = element.column.split(":")[1];
+        obj.count = element.$;
+        result[element.key].push(obj);
+      });
+      console.log(result);
+      res.status(200).send(result);
       if (err) {
         res.sendStatus(500);
       }
